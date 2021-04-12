86cf07dc425a3f05f2120b60e0049441e936cf39
Orlando-R <roglorand@gmail.com>
2021-04-12 15:44:49 +0200

    Harmadik gyakorlatom

---
+++ GitGyakorlas3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitGyakorlas3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("gyakorlojuk 3");
+        }
+    }
+}
